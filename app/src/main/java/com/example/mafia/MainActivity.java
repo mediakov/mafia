9c48b7a1048674ef6e32621c6dd8762043a33618
mediakov <mediakov@gmail.com>
2015-06-04 15:26:12 +0300

    Trying to add dialog with two EditTexy Views.

--- app/src/main/java/com/example/mafia/MainActivity.java
+++ app/src/main/java/com/example/mafia/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,28 +30,45 @@ public class MainActivity extends AppCompatActivity {
         final CardAdapter cardAdapter = new CardAdapter(this, playerArray);
         gridView.setAdapter(cardAdapter);
 
-        final MaterialDialog.InputCallback inputCallback = new MaterialDialog.InputCallback() {
-            public void onInput(MaterialDialog dialog, CharSequence playerName) {
-                playerArray.add(playerName.toString());
+        final MaterialDialog.ButtonCallback inputCallback = new MaterialDialog.ButtonCallback() {
+            public void onPositive(MaterialDialog dialog) {
+                LinearLayout playerData = new LinearLayout(dialog.getContext());
+                EditText playerName = new EditText(dialog.getContext());
+                playerData = (LinearLayout) dialog.getCustomView();
+                playerName = (EditText) playerData.getChildAt(1);
+                playerArray.add(playerName.getText().toString());
                 cardAdapter.notifyDataSetChanged();
                 gridView.smoothScrollToPosition(i);
+            }
+
+            public void onNegative(MaterialDialog dialog) {
+                dialog.dismiss();
             }
         };
         final View.OnClickListener regFabListener = new View.OnClickListener() {
             public void onClick(final View v) {
-
+                Log.d("MY_LOG", "Начата генерация диалога");
                 final MaterialDialog.Builder builder = new MaterialDialog.Builder(v.getContext());
                 builder.title(R.string.registration_player_dialog_title);
                 builder.content(R.string.registration_player_dialog_content);
+                Log.d("MY_LOG", "Установлены title и content");
                 LinearLayout playerRegFieldsHolder = new LinearLayout(v.getContext());
-                EditText playerNumber = new EditText(v.getContext());
+/*                EditText playerNumber = new EditText(v.getContext());
                 playerNumber.setInputType(2);
                 EditText playerName = new EditText(v.getContext());
                 playerRegFieldsHolder.addView(playerNumber);
-                playerRegFieldsHolder.addView(playerName);
-                builder.customView(playerRegFieldsHolder, false);
+                playerRegFieldsHolder.addView(playerName);*/
+                builder.customView(R.layout.player_registration_dialog, true);
+                Log.d("MY_LOG", "Добавлен layout c формой");
+                builder.positiveText(android.R.string.ok);
+                builder.negativeText(android.R.string.cancel);
+                Log.d("MY_LOG", "Установлены кнопки");
+                builder.callback(inputCallback);
+                Log.d("MY_LOG", "Установлен callback");
                 MaterialDialog playerRegistrationDialog = builder.build();
+                Log.d("MY_LOG", "Построен диалог");
                 playerRegistrationDialog.show();
+
             }
         };
         FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.FloatingActionButtonID);
